dc21a4a045ee6bc6d2b3140bb25d95a90b3873a9
Shubhradeep Bose <shubhradeepbose79@gmail.com>
2024-09-06 23:41:20 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,15 +3,6 @@ import BlogList from "./components/BlogList";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
-// export default function Home() {
-//   return (
-//    <>
-
-// >>>>>>> cc5ee292923787bf27e49ed5bf48ee44c22b0687
-//    </>
-//   );
-// }
-
 const Header = () => {
       return (
             <>
