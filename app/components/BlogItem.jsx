48f10835f6bb5a82195fb5b3e7366d293bf27d68
Shubhradeep Bose <shubhradeepbose79@gmail.com>
2024-09-06 23:48:18 +0530

    Update BlogItem.jsx

--- app/components/BlogItem.jsx
+++ app/components/BlogItem.jsx
@@ -11,17 +11,17 @@ const BlogItem = ({image, tittle, description, category, id}) => {
       </Link>
       <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-small'> {category} </p> {/* Here have to display blog catagory */} 
       <div className="p-5">
-<<<<<<< HEAD
+
         <h5 className='mb-2 text-lg font-medium tracking-tight text-grey-900'>{tittle}</h5> {/* Here have to add blog tittle */}
           <p className='mb-3 text-sm tracking-tight text-grey-700 '>{description}</p>{/* Here have to add blog Description */}
           <Link href={`/blogs/${id}`} className='inline-flex items-center py-2 font-semibold text-center '>
             Read More <Image className='ml-2'  width={12}/> {/* add arrow icon  */}
-=======
+          </Link>
         <h5 className='mb-2 text-lg font-medium tracking-tight text-grey-900'>{blogData[0].tittle}</h5> {/* Here have to add blog tittle */}
           <p className='mb-3 text-sm tracking-tight text-grey-700 '>{blogData[0].description}</p>{/* Here have to add blog Description */}
-          <div className='inline-flex items-center py-2 font-semibold text-center '>
+          <Link className='inline-flex items-center py-2 font-semibold text-center '>
             Read More More <Image className='ml-2'  width={12}/> {/* add arrow icon  */}
->>>>>>> cc5ee292923787bf27e49ed5bf48ee44c22b0687
+
             
           </Link>
       </div>
