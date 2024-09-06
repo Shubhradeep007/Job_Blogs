d3da0843251704eb032c18ec835f81b73c62e9b8
Shubhradeep Bose <shubhradeepbose79@gmail.com>
2024-09-06 23:39:42 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,18 +3,24 @@ import BlogList from "./components/BlogList";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
-export default function Home() {
-  return (
-   <>
-      < Header />
-<<<<<<< HEAD
-      <BlogList />
-      <Footer />
-=======
-      
-      <BlogList />
-      < BlogItem />
->>>>>>> cc5ee292923787bf27e49ed5bf48ee44c22b0687
-   </>
-  );
-}
+// export default function Home() {
+//   return (
+//    <>
+
+// >>>>>>> cc5ee292923787bf27e49ed5bf48ee44c22b0687
+//    </>
+//   );
+// }
+
+const Header = () => {
+      return (
+            <>
+                  < Header />
+                  <BlogList />
+                  <Footer />
+                  <BlogList />
+                  < BlogItem />
+            </>
+      )
+};
+export default Header;
